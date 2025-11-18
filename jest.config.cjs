b16b1aa741e70af09a34a15dde41b50e38355aa4
JonasBa <jonas@badalic.com>
2025-11-18 09:59:02 -0800

    jest: fix esm testing

---
+++ jest.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                useESM: true,
+            },
+        ],
+    },
+};
